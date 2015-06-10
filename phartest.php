ce5ef9f0c18c7d9d522692d85a3d01302069cb27
Nigel Atkinson <nigel@kinson.kiwi>
2015-06-10 17:03:26 +1000

    PharBuilder is now in in a working state.

---
+++ phartest.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+require_once( 'autoloader.php' );
+
+use NigeLib\Console;
+
+Console::important( 'Test script for NigeLib PharBuilder.' );
+
+$pb = new NigeLib\PharBuilder( 'test.phar', 'test.php', dirname( __FILE__ ) );
+
+$pb->addExcludePrefix( '/.git' );
+$pb->addExcludePrefix( '/composer' );
+$pb->addExcludePrefix( '/phartest.php' );
+$pb->addExcludeSuffix( '~' );
+
+$pb->compile();
