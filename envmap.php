7c78cc457edfd9a8bf3981dd8555d3d91d9bbecf
Nigel Atkinson <nigel@kinson.kiwi>
2015-06-15 15:58:33 +1000

    Move environment definition out of src directory.

---
+++ envmap.php
@@ -0,0 +1,12 @@
+<?php
+// Environment map.
+//
+// Maps environment names to one or more host names.
+// These can be used by the Config class via the Environment class
+// to read different configurations depending on which host this is running
+// on.
+
+return array(
+    'live' => array( 'atkinson.kiwi' ),
+    'test' => array( 'supra' ),
+);
