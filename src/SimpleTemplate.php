71a3e7ea6f83a1242053e068f217d98b08a90572
Nigel Atkinson <nigel@kinson.kiwi>
2015-10-19 17:24:59 +1100

    Added access methods for SimpleTemplate

--- src/SimpleTemplate.php
+++ src/SimpleTemplate.php
@@ -1,6 +1,9 @@
 <?php namespace NigeLib;
 
-class SimpleTemplate
+use \ArrayAccess;
+use \Exception;
+
+class SimpleTemplate implements ArrayAccess
 {
     private $filename;
     private $parameters = array();
@@ -22,6 +25,43 @@ class SimpleTemplate
         $parameters = array_merge( $this->parameters, $extra_parameters );
 
         return SimpleTemplateRender( $this->filename, $parameters );
+    }
+
+    // ArrayAccess
+    public function offsetSet( $offset, $value ) {
+        if( is_null( $offset ) ) {
+            throw new Exception( "Parameters must have keys" );
+        }
+        $this->parameters[$offset] = $value;
+    }
+
+    public function offsetExists( $offset ) {
+        return isset( $this->parameters[$offset] );
+    }
+
+    public function offsetUnset( $offset ) {
+        unset( $this->parameters[$offset] );
+    }
+
+    public function offsetGet( $offset ) {
+        return isset( $this->parameters[$offset] ) ? $this->parameters[$offset] : null;
+    }
+
+    // Object Style
+    public function &__get( $key ) {
+        return $this->parameters[$key];
+    }
+
+    public function __set( $key, $value ) {
+        $this->parameters[$key] = $value;
+    }
+
+    public function __isset( $key ) {
+        return isset( $this->parameters[$key] );
+    }
+
+    public function __unset( $key ) {
+        unset( $this->parameters[$key] );
     }
 }
 
