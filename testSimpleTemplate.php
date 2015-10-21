72676f821d888682bb6df463430fe009c9caba57
Nigel Atkinson <nigel@kinson.kiwi>
2015-10-21 13:42:02 +1100

    Templates do not need to be files.
    Also the code is 'cached' as long as the instance is alive. Code could
    come from DB, or hardcoded.

--- testSimpleTemplate.php
+++ testSimpleTemplate.php
@@ -6,7 +6,16 @@ use NigeLib\SimpleTemplate;
 $main = new SimpleTemplate( 'main.tpl' );
 
 $main->title = 'Motorbike clothing for sale';
-$main->items = new SimpleTemplate( 'items.tpl' );
+
+$main->items = new SimpleTemplate( null, '
+<ul>
+ <?php foreach( $itemlist as $item=>$price ): ?>
+  <?=$itemlist_tpl->render( compact( "item", "price" ) ) ?>
+ <?php endforeach; ?>
+</ul>' );
+
 $main->items->itemlist = array( 'Boots' => 230, 'Jackets' => 100, 'Gloves' => 50, 'Helmets' => 250 );
+
+$main->items->itemlist_tpl = new SimpleTemplate( null, '<li><?=$item?> - from $<?=number_format($price,2)?></li>' );
 
 echo $main;
